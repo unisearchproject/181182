3de832ad363aaf618cf02ce50c92e6f7b5a3e360
ninarzum <51048784+ninarzum@users.noreply.github.com>
2019-06-10 11:04:03 +0300

    imports

---
+++ private.py
@@ -0,0 +1,12 @@
+# токен бота от BotFather
+TOKEN = 
+
+# используйте прокси для работы бота
+PROXY = 
+
+# адрес подключения к mongodb клиенту
+# этот означает базу данных на нашем компьютере
+MONGO_CLIENT = 'mongodb://localhost:27017/'
+
+# название базы данных
+MONGO_DATABASE = 'teamproject_test'
